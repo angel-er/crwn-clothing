b1b520cc11a4617157a4a37c9dc9f3786a74029e
root <angele.educar@gmail.com>
2019-10-16 09:44:08 -0300

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.comopnent';
+import HomePage from './pages/homepages/homepages.component';
 
 function App() {
   return (
